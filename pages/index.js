fdd8a75142df1f02e108c1c897b21a1345fadadd
Akuoko Daniel Jnr <danielakuokojnr@gmail.com>
2022-03-30 18:19:36 +0000

    feat: add hero texts

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,48 @@
+import { css } from "@emotion/react";
+
+const STYLES_CONTAINER = css`
+  max-width: 1340px;
+  margin-inline: auto;
+  padding: 0 1.5rem;
+`;
+
+const STYLES_SECTION = css`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(30rem, 1fr));
+  align-items: center;
+  min-height: 100vh;
+`;
+
+const STYLES_TITLE = css`
+  font-size: clamp(2rem, 1.1vw + 1.8rem, 2.8rem);
+  font-weight: 700;
+  line-height: 3.6rem;
+`;
+
+const STYLES_SUBTITLE = css`
+  background: linear-gradient(
+    180deg,
+    rgba(175, 175, 175, 1) 0%,
+    rgba(213, 213, 213, 1) 75%
+  );
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+`;
+
 export default function Home() {
   return (
-    <div>
-      <p>Aide</p>
-    </div>
+    <main css={STYLES_CONTAINER}>
+      <section css={STYLES_SECTION}>
+        <div>
+          <div>
+            <h3 css={STYLES_TITLE}>A new day for customer support.</h3>
+            <h3 css={[STYLES_TITLE, STYLES_SUBTITLE]}>
+              Share knowledge <br /> Automate common tasks <br /> Better
+              understand team productivity
+            </h3>
+          </div>
+        </div>
+      </section>
+    </main>
   );
 }
