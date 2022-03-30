e3987a56193f55001c2bbbfa3ab9e639efaae4bf
Akuoko Daniel Jnr <danielakuokojnr@gmail.com>
2022-03-30 16:29:27 +0000

    chore: project set-up

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import { Global } from "@emotion/react";
+
+import injectGlobalStyles from "common/styles/global";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Global styles={injectGlobalStyles()} />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
