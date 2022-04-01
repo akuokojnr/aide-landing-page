cfff18394a21ee1e046c3bf1ff8e61933494468b
Akuoko Daniel Jnr <danielakuokojnr@gmail.com>
2022-04-01 09:02:38 +0000

    feat: add uses

--- pages/index.js
+++ pages/index.js
@@ -71,6 +71,96 @@ const STYLES_BUTTONS = css`
   }
 `;
 
+const STYLES_USES_TITLE = css`
+  font-size: 1.4rem;
+  line-height: 1.8rem;
+  font-weight: 600;
+  color: ${Constants.colors.black100};
+`;
+
+const STYLES_USES = css`
+  margin: 11rem 0 0;
+  padding: 3rem 0;
+  border-top: 1px solid ${Constants.colors.gray400};
+`;
+
+const STYLES_USE_WRAPPER = css`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 0.7rem;
+  margin: 2.4rem 0 0;
+`;
+
+const STYLES_USE = css`
+  background: ${Constants.colors.gray500};
+  border-radius: 10rem;
+  padding: 0.5rem 1.2rem;
+  display: flex;
+  align-items: center;
+  font-size: clamp(1.1rem, 0.4vw + 1rem, 1.4rem);
+  line-height: 1.8rem;
+  font-weight: 600;
+  color: ${Constants.colors.black200};
+
+  span {
+    display: inline-block;
+    margin-left: 1rem;
+  }
+
+  svg {
+    width: clamp(2rem, 0.5vw + 1.9rem, 2.4rem);
+    height: clamp(2rem, 0.5vw + 1.9rem, 2.4rem);
+  }
+
+  svg.bag {
+    width: clamp(1.2rem, 0.5vw + 1.1rem, 1.6rem);
+    height: clamp(1.4rem, 0.5vw + 1.3rem, 1.8rem);
+  }
+`;
+
+const USES = [
+  {
+    text: "Search across internal tools",
+    icon: <SVG.Sailboat />,
+  },
+  {
+    text: "Intelligent suggestions",
+    icon: <SVG.Stars />,
+  },
+  {
+    text: "Share text snippets and responses",
+    icon: <SVG.Snippet />,
+  },
+  {
+    text: "Codify knowledge with processes",
+    icon: <SVG.Puzzle />,
+  },
+  {
+    text: "Connect your knowledge base",
+    icon: <SVG.Library />,
+  },
+  {
+    text: "Easily visualize and understand data",
+    icon: <SVG.Kind />,
+  },
+  {
+    text: "E-commerce actions",
+    icon: <SVG.Bag className="bag" />,
+  },
+  {
+    text: "Works with the tools you already use",
+    icon: <SVG.Inbox />,
+  },
+  {
+    text: "Turn multi-step operations into a single click",
+    icon: <SVG.Hare />,
+  },
+  {
+    text: "Lots more, coming soon.",
+    icon: "+",
+  },
+];
+
 export default function Home() {
   const [isFormActive, setIsFormActive] = useState(false);
 
@@ -111,6 +201,21 @@ export default function Home() {
           </div>
 
           {isFormActive && <ContactForm />}
+
+          <div css={STYLES_USES}>
+            <p css={STYLES_USES_TITLE}>
+              Save time, reduce costs and increase both customer <em>and</em>{" "}
+              agent happiness.
+            </p>
+            <div css={STYLES_USE_WRAPPER}>
+              {USES.map(({ icon, text }, index) => (
+                <div key={`uses-${++index}`} css={STYLES_USE}>
+                  {icon}
+                  <span>{text}</span>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
       </section>
     </main>
